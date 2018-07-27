8a06dafb6bd0a412b82d563e26c910904be002b9
Mallerie Shirley <mallerie@gmail.com>
2018-07-27 08:19:14 -0400

    Removed fetch call since the data is not usable from the api

--- src/App/App.js
+++ src/App/App.js
@@ -2,17 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 
-class App extends Component {
-  componentWillMount() {
-    fetch('https://api.github.com/gists/ec791a5c4cc6507f7c814b24a5ac750b')
-      .then(results => {
-        return results.json();
-      })
-      .then(data => {
-        console.log(data.files['clusters.json'].content);
-      });
-  }
-
+class App extends Component {  
   render() {
     return (
       <div className="App">
