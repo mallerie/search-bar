51333593fc600030ed9a79c735a4d3f314330236
Mallerie Shirley <mallerie@gmail.com>
2018-07-28 20:18:29 -0400

    Move search functionality into util and move state into parent component

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
+import Dropdown from '../Dropdown/Dropdown';
+import searchData from '../utils/searchData.js';
 
-class App extends Component {  
+
+export default class App extends Component { 
+  constructor(props) {
+		super(props);
+    
+    this.state = {
+      queryResults: []
+    };
+  }
+  
+  handleChange = (event) => {    
+    this.setState({
+      queryResults: searchData(event.target.value)
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <SearchBar />
+        <SearchBar onChange={this.handleChange} /> 
+        <Dropdown list={this.state.queryResults} />
       </div>
     );
   }
 }
-
-export default App;
--- src/SearchBar/SearchBar.js
+++ src/SearchBar/SearchBar.js
@@ -1,63 +1,24 @@
 import React, { Component } from 'react';
-import data from '../data/clusters.json'
+import PropTypes from "prop-types";
 
 export default class SearchBar extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			query: '',
-			data: data,
-			queryResults: [],
-			lowercaseData: []
-		};
-	}
-
-	componentWillMount() {
-		let lowercaseData = data.map(item => {
-			return ({ id: item.id, name: item.name.toLowerCase() })
-		});
-
-		this.setState({
-			lowercaseData: lowercaseData
-		});
-	}
-
 	handleChange = (event) => {
-		const query = event.target.value;
-		this.setState({ query: event.target.value });
-
-		// if (searchQuery.length > 2) {
-			this.searchData(query);
-		// }
-	}
-
-	searchData = (query) => {
-		let lowercaseData = this.state.lowercaseData;
-		let cleanQuery = query.toLowerCase();
-		let queryResults = [];
-
-		for (let i = lowercaseData.length; i--;) {
-			if (this.state.lowercaseData[i].name.indexOf(cleanQuery) !== -1) {
-				queryResults.push(this.state.data[i]);
-				this.setState({queryResults: queryResults});
-			}
-			else {
-				//If user changes query, remove item from array
-	
-			}
-		}
-	}
+    this.props.onChange(event);
+  };
 
 	render() {
 		return (
 			<div>
 				<input
 					type="text"
-					placeholder="search"
-					value={this.state.query}
+					placeholder={"Search by cluster id or name ..."}
+					value={this.props.query}
 					onChange={this.handleChange} />
 			</div>
 		);
 	}
+}
+
+SearchBar.propTypes = {
+	onChange: PropTypes.func
 }
