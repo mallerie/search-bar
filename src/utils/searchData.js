345a90f1cca33c18d25892e7a6f0a393ff789140
Mallerie Shirley <mallerie@gmail.com>
2018-07-28 20:14:57 -0400

    Move search functionality to utils, return empty array if there is no query

---
+++ src/utils/searchData.js
@@ -0,0 +1,21 @@
+import data from '../data/clusters'
+
+export default function searchData(query) {
+  let lowercaseData = data.map(item => {
+			return ({ id: item.id, name: item.name.toLowerCase() })
+  });
+  let queryResults = [];
+  let emptyArray = [];
+
+  for (let i = 0; i < lowercaseData.length; i++) {
+   if (lowercaseData[i].name.indexOf(query.toLowerCase()) !== -1) {
+    queryResults.push(data[i]);
+   }
+  }
+
+  if (!query) {
+   queryResults = emptyArray;
+  }
+
+  return queryResults;
+}
