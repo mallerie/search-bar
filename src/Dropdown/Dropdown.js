8b220ea242de0f9e67fd0d85f505e953a71c49e6
Mallerie Shirley <mallerie@gmail.com>
2018-07-29 09:27:43 -0400

    Add constructor with state and pass selected result to the display component

--- src/Dropdown/Dropdown.js
+++ src/Dropdown/Dropdown.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import Display from '../Display/Display';
 
 export default class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showSelectedResult: false
+    };
+  }
+
+  shortenId = (id) => {
+    let shortId = id.slice(0, 6);
+    return shortId
+  }
+
+  handleSelectResult = (item) => {
+    this.setState({
+      showSelectedResult: true,
+      result: item
+    });
+  }
+
   render() {
+    let { showSelectedResult, result } = this.state;
     return (
-      <ul className="dropdown">
-        {this.props.list.map(listItem => {
-          return <li>{listItem.name}</li>
-        })}
-      </ul>
+      <div>
+        <ul className="dropdown">
+          {this.props.list.map(listItem => {
+            return <li onClick={() => this.handleSelectResult(listItem)} key={listItem.id}>{listItem.name}({this.shortenId(listItem.id)})</li>
+          })}
+        </ul>
+        {showSelectedResult &&
+          <Display
+            title={"Selected Cluster"}
+            name={result.name}
+            id={result.id}
+          />}
+      </div>
     );
   }
 }
