1c3c59d22937b697d5d80f20d1301085d9de52c8
Mallerie Shirley <mallerie@gmail.com>
2018-08-01 18:28:43 -0400

    Added result prop to pass selected item data to Display

--- src/Display/Display.js
+++ src/Display/Display.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 
 export default class Display extends Component {
- render() {
-  return(
-    <div>
-     <div>{this.props.title}</div>
-     <div>{this.props.name}</div>
-     <div>{this.props.id}</div>
-     <div><button onClick={() => alert("change clicked")}>Change</button></div>
-    </div>
-  );
- }
+  handleClearApp = () => {
+    this.props.onClick();
+  }
+
+  render() {
+    let { result } = this.props;
+
+    return (
+      <div>
+        <div>{this.props.title}</div>
+        <div>{result.name}</div>
+        <div>{result.id}</div>
+        <div><button onClick={this.handleClearApp}>Change</button></div>
+      </div>
+    );
+  }
 }
