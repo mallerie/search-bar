efee9e81066cc72ddb641cb07ffd462ac88e79f9
Mallerie Shirley <mallerie@gmail.com>
2018-08-01 20:30:19 -0400

    Add shortenId method & classNames for styling

--- src/Display/Display.js
+++ src/Display/Display.js
@@ -5,15 +5,21 @@ export default class Display extends Component {
     this.props.onClick();
   }
 
+  shortenId = (id) => {
+    let shortId = id.slice(0, 6);
+    return shortId
+  }
+
   render() {
     let { result } = this.props;
 
     return (
-      <div>
-        <div>{this.props.title}</div>
-        <div>{result.name}</div>
-        <div>{result.id}</div>
-        <div><button onClick={this.handleClearApp}>Change</button></div>
+      <div className="display">
+        <div className="title-name">
+          <div className="title">{this.props.title}</div>
+          <div className="result">{result.name}  ({this.shortenId(result.id)})</div>
+        </div>  
+        <button onClick={this.handleClearApp}>Change</button>
       </div>
     );
   }
